993279a49047cc9503690359fce1d84c9c9c5af5
ParthKaretiya <parth.karetiya.cg@gmail.com>
2026-04-01 10:31:39 +0530

    cpp

---
+++ CW/March/31/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {15,2,3,6,7};
+    string num = "";
+    int res;
+
+    for(int i = 0; i < 5; i++) {
+        num = num + to_string(arr[i]);  
+    }
+    res = stoi(num)+1;
+    // res = res + 1;
+    // fd
+   
+
+    cout << res<< endl;  
+}
